b3b59d0f08ad038045683e44957fc522a84380d6
Taekhwan Han <hth3396@gmail.com>
2023-03-03 00:57:35 +0900

    feat: 이미지 받아올 URLSession 메소드 구현

--- Wanted_PreOnboarding_ImageDownload/Wanted_PreOnboarding_ImageDownload/Services/NetworkService.swift
+++ Wanted_PreOnboarding_ImageDownload/Wanted_PreOnboarding_ImageDownload/Services/NetworkService.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 final class NetworkService {
-    func getImages() {
-        
+    func getImage(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func downloadImage(from url: URL) {
+        getImage(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async() { [weak self] in
+                //TODO: 이미지 지정 들어갈 부분
+            }
+        }
     }
 }
