dddd6454c1027370f732ea95c98cb845efe8719d
Taekhwan Han <hth3396@gmail.com>
2023-03-02 20:51:02 +0900

    init project

---
+++ Wanted_PreOnboarding_ImageDownload/Wanted_PreOnboarding_ImageDownload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanted_PreOnboarding_ImageDownload
+//
+//  Created by 한택환 on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
