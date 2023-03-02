80f7d032993573931ef6ac6d6628a334cc6ea0cd
Taekhwan Han <hth3396@gmail.com>
2023-03-02 21:09:48 +0900

    feat: 문서 구조 작성

---
+++ Wanted_PreOnboarding_ImageDownload/Wanted_PreOnboarding_ImageDownload/Services/NetworkService.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkService.swift
+//  Wanted_PreOnboarding_ImageDownload
+//
+//  Created by 한택환 on 2023/03/02.
+//
+
+import Foundation
+
+final class NetworkService {
+    func getImages() {
+        
+    }
+}
